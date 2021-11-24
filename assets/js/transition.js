f57db6f86cc05ba2ee4a1b6864eea637ff744bd0
somya339999 <somyagupta746@gmail.com>
2021-11-24 11:06:23 +0530

    Torch effect added on hero element

--- assets/js/transition.js
+++ assets/js/transition.js
@@ -1,2 +1,15 @@
-outClass = 'pt-page-scaleDown';
-inClass = 'pt-page-scaleUpDown pt-page-delay300';
+const hero = document.querySelector("#hero");
+
+hero.addEventListener("mousemove", e => {
+    // e.target.style.clipPath = `ellipse(60% ${e.y*2}% at ${e.x/8}% 0%)`
+})
+const trackMouse = (e) => {
+   hero.style.setProperty(
+        '--cursorXpos', `${e.clientX}px`
+    )
+    hero.style.setProperty(
+        '--cursorYpos', `${e.clientY}px`
+    )
+}
+hero.addEventListener('mousemove' , trackMouse);
+
